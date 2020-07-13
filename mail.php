8f5ffb029fda4e468602273489d3e455da6e41f0
Zelenyj <61359697+Zelenyj@users.noreply.github.com>
2020-07-13 15:55:58 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $email = $_POST['email'];
 
 
 $content="From: $name";
-$recipient = "daniel@byol.ie";
+$recipient = "bike@sample.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
